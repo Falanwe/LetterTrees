c33d91615e3eb13adcc5e21bbced94ef3b443c0c
Jean-Noël Gourdol <jngourdol@stormancer.com>
2022-02-02 11:04:43 +0100

    Creates project

---
+++ LetterTrees/Models/LetterTree.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetterTrees.Models
+{
+    public class LetterTree
+    {
+        public char Letter { get; }
+
+        public List<LetterTree> Children { get; } = new List<LetterTree>();
+    }
+}
---
+++ LetterTrees/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LetterTrees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
